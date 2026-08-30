9403ee78000ad6383c544d45f827ff6d64b851ca
wooil1964 <wooil2000@gmail.com>
2026-08-30 10:12:09 +0900

    유부도 조석 관측소명을 군산외항으로 변경

--- .github/scripts/update_tide.py
+++ .github/scripts/update_tide.py
@@ -49,6 +49,7 @@ ADDITIONAL_TIDE_SITES = (
 )
 FORECAST_STATION_OVERRIDES = {
     "17": ("안흥", "DT_0067"),
+    "19": ("군산외항", "DT_0018"),
     "49": ("울산", "DT_0020"),
     "107": ("궁평항", "SO_1268"),
 }
